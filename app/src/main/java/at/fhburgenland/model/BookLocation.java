c5a29a298d579146f3fdb5690e82f7909e15a5bb
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-06-10 07:35:50 +0200

    add book location + shelf + floor entities

---
+++ app/src/main/java/at/fhburgenland/model/BookLocation.java
@@ -0,0 +1,36 @@
+package at.fhburgenland.model;
+
+import jakarta.persistence.*;
+
+/**
+ * Book Location Entity - represents a physical location of a
+ * physical copy of a book (whereas a book is represented by an ISBN)
+ */
+@Entity(name = "BookLocation")
+@Table(name = "book_location")
+public class BookLocation {
+    @Id
+    @GeneratedValue
+    @Column(name = "book_location_id", updatable = false, nullable = false)
+    private Integer bookLocationId;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "book_location_floor_id", nullable = false)
+    private BookLocationFloor bookLocationFloor;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "book_location_shelf_id", nullable = false)
+    private BookLocationShelf bookLocationShelf;
+
+    public Integer getBookLocationId() {
+        return this.bookLocationId;
+    }
+
+    public void setBookLocationFloor(BookLocationFloor bookLocationFloor) {
+        this.bookLocationFloor = bookLocationFloor;
+    }
+
+    public void setBookLocationShelf(BookLocationShelf bookLocationShelf) {
+        this.bookLocationShelf = bookLocationShelf;
+    }
+}
