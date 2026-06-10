ff76b8d24c7f0035a27f59818f054443d05ec84f
strawberryTaiquiri <2510859005@hochschule-burgenland.at>
2026-06-10 22:03:08 +0200

    add streetdao

---
+++ app/src/main/java/at/fhburgenland/model/dao/implementations/StreetDaoImpl.java
@@ -0,0 +1,48 @@
+package at.fhburgenland.model.dao.implementations;
+
+import at.fhburgenland.model.Street;
+
+import at.fhburgenland.model.dao.interfaces.StreetDao;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+/**
+ * Concrete implementation of Street DAO - offers CRUD operations
+ */
+public class StreetDaoImpl implements StreetDao {
+    private final EntityManager entityManager;
+
+    public StreetDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void create(Street model) {
+        entityManager.persist(model);
+    }
+
+    @Override
+    public Street readById(Integer id) {
+        return entityManager.find(Street.class, id);
+    }
+
+    @Override
+    public List<Street> readByName(String name) {
+        String query = "select street from Street street where lower(street.street) = lower(:name)";
+        TypedQuery<Street> typedQuery = entityManager.createQuery(query, Street.class);
+        typedQuery.setParameter("name", name);
+        return typedQuery.getResultList();
+    }
+
+    @Override
+    public void update(Street model) {
+        entityManager.merge(model);
+    }
+
+    @Override
+    public void delete(Street model) {
+        entityManager.remove(model);
+    }
+}
---
+++ app/src/main/java/at/fhburgenland/model/dao/interfaces/StreetDao.java
@@ -0,0 +1,20 @@
+package at.fhburgenland.model.dao.interfaces;
+
+import at.fhburgenland.model.Street;
+
+import java.util.List;
+
+/**
+ * Abstract class that provides CRUD operations for Street
+ */
+public interface StreetDao {
+    void create(Street model);
+
+    Street readById(Integer id);
+
+    List<Street> readByName(String name);
+
+    void update(Street model);
+
+    void delete(Street model);
+}
