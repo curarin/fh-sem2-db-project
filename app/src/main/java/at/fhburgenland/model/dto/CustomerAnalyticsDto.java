ea5b6dd80cbd2289a8db7770808795d772e0bc76
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-06-17 05:39:21 +0200

    add countTotalActivities to DTO

--- app/src/main/java/at/fhburgenland/model/dto/CustomerAnalyticsDto.java
+++ app/src/main/java/at/fhburgenland/model/dto/CustomerAnalyticsDto.java
@@ -6,8 +6,9 @@ import at.fhburgenland.model.Customer;
  * DTO for analytics query which holds specifically aggregated input from db
  *
  * @param customer           Customer object
- * @param countLoanedBooks   Number of books at loan
+ * @param countBooksAtLoan   Number of books at loan
  * @param countVisitedEvents number of visited events
+ * @param countTotalActivities sum of borrowed books + visited events
  */
-public record CustomerAnalyticsDto(Customer customer, Integer countLoanedBooks, Integer countVisitedEvents) {
+public record CustomerAnalyticsDto(Customer customer, Integer countBooksAtLoan, Integer countVisitedEvents, Integer countTotalActivities) {
 }
